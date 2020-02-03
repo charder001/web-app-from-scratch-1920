30c790ea6b81d763b9321378ad5f90ab33c0e3d6
charder001 <rickdchardet@gmail.com>
2020-02-03 15:45:09 +0100

    Added API call and converted Fahrenheit to celcius

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,10 +1,25 @@
-const fetch = require("node-fetch");
-
-fetch('https://api.darksky.net/forecast/69c6fee3ced588bd9f706e41453643c8/52.308262, 5.054286')
+fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/69c6fee3ced588bd9f706e41453643c8/52.308262, 5.054286?units=si')
   .then((response) => {
     return response.json();
   })
+
+
+  .then((myJson) => {
+    console.log(myJson);
+    var para = document.createElement("p");
+    var node = document.createTextNode(myJson.currently.temperature);
+    para.appendChild(node);
+    var element = document.getElementById("div1");
+    element.appendChild(para);
+  });
+
+/*
   .then((myJson) => {
     console.log(myJson);
   });
-
+*/
+/*var para = document.createElement("p")
+var text = document.createTextNode("Hoi")
+createp.appendChild(text);
+document.getElementById("temp")
+*/

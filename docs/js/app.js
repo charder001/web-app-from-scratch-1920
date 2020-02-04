d001066490c286af5028f2132055b8592a66c9be
Charder001 <rick.chardet@hva.nl>
2020-02-04 13:47:28 +0100

    Fixed request error

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,8 +1,22 @@
-fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/69c6fee3ced588bd9f706e41453643c8/52.308262, 5.054286?units=si')
+// fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/39269c07a47ba71c7c9af7ee9e8415fd/52.308262, 5.054286?units=si')
+//   .then((response) => {
+//     return response.json();
+//   })
+
+//   .then((myJson) => {
+//     console.log(myJson);
+//     var para = document.createElement("p");
+//     var node = document.createTextNode(myJson.currently.temperature);
+//     para.appendChild(node);
+//     var element = document.getElementById("div1");
+//     element.appendChild(para);
+//   })
+
+  
+fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/39269c07a47ba71c7c9af7ee9e8415fd/37.8267,-122.4233`)
   .then((response) => {
     return response.json();
   })
-
 
   .then((myJson) => {
     console.log(myJson);
@@ -11,15 +25,15 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/69c6
     para.appendChild(node);
     var element = document.getElementById("div1");
     element.appendChild(para);
-  });
+  })
 
-/*
-  .then((myJson) => {
-    console.log(myJson);
-  });
-*/
-/*var para = document.createElement("p")
-var text = document.createTextNode("Hoi")
-createp.appendChild(text);
-document.getElementById("temp")
-*/
+    // .then((myJson) => {
+  //   console.log(myJson);
+  //   var para = document.createElement("p");
+  //   var node = document.createTextNode(myJson.currently.temperature);
+  //   para.appendChild(node);
+  //   var element = document.getElementById("div1");
+  //   element.appendChild(para);
+  // });
+
+  

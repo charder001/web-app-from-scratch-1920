36b3fda99e3403aef281233e133e2662a806915d
Charder001 <rick.chardet@hva.nl>
2020-02-04 15:16:26 +0100

    Added error handling and expanded on the displayed amount of data using objects, arrays and object entries

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,39 +1,29 @@
-// fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/39269c07a47ba71c7c9af7ee9e8415fd/52.308262, 5.054286?units=si')
-//   .then((response) => {
-//     return response.json();
-//   })
-
-//   .then((myJson) => {
-//     console.log(myJson);
-//     var para = document.createElement("p");
-//     var node = document.createTextNode(myJson.currently.temperature);
-//     para.appendChild(node);
-//     var element = document.getElementById("div1");
-//     element.appendChild(para);
-//   })
-
-  
-fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/39269c07a47ba71c7c9af7ee9e8415fd/37.8267,-122.4233`)
+fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/39269c07a47ba71c7c9af7ee9e8415fd/37.8267,-122.4233?units=si`)
   .then((response) => {
+    if (response.ok)  {
     return response.json();
+    } else {
+      return Promise.reject(response)
+    }
   })
 
   .then((myJson) => {
     console.log(myJson);
-    var para = document.createElement("p");
-    var node = document.createTextNode(myJson.currently.temperature);
-    para.appendChild(node);
-    var element = document.getElementById("div1");
-    element.appendChild(para);
+    var weatherValues = {
+      temperature: myJson.currently.temperature,
+      precipIntensity: myJson.currently.precipIntensity,
+      precipProbability: myJson.currently.precipProbability
+    }
+
+    for (let [key, value] of Object.entries(weatherValues)){
+      console.log(`${key}: ${value}`)
+      var p = document.getElementById("p1")
+      p.insertAdjacentHTML("afterend",  `<p>${key}: ${value}</p>`)
+    }
+
+  }) .catch ((err) => {
+    console.log("An error occured - ", err)
   })
 
-    // .then((myJson) => {
-  //   console.log(myJson);
-  //   var para = document.createElement("p");
-  //   var node = document.createTextNode(myJson.currently.temperature);
-  //   para.appendChild(node);
-  //   var element = document.getElementById("div1");
-  //   element.appendChild(para);
-  // });
 
   
